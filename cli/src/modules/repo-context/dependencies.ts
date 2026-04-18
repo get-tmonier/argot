a3aeb9f2c06b29dd9e4a26581c73e1a073453daf
damienmeur <damien.meur@tmonier.com>
2026-04-18 10:05:53 +0200

    feat(repo-context): module dependencies export

---
+++ cli/src/modules/repo-context/dependencies.ts
@@ -0,0 +1,4 @@
+import { FsRepoContextLive } from '#modules/repo-context/infrastructure/adapters/out/fs-repo-context.adapter.ts';
+
+export { RepoContext } from '#modules/repo-context/application/ports/out/repo-context.port.ts';
+export const RepoContextLive = FsRepoContextLive;
